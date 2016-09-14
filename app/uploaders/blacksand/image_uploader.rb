e593a24ef7f8f4d42705fecc0a91eba87f10c470
xielinxu <799104846@qq.com>
2016-09-14 11:29:09 +0800

    test new version

--- app/uploaders/blacksand/image_uploader.rb
+++ app/uploaders/blacksand/image_uploader.rb
@@ -60,7 +60,7 @@ module Blacksand
     version :larger do
       process :resize_to_fill => [224,148]
     end
-    
+
 
     # Add a white list of extensions which are allowed to be uploaded.
     # For images you might use something like this:
